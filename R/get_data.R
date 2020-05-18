63dc0074039f43278156317304521874a65154b6
Tamas Kiss <tamas-kiss@ouhsc.edu>
2020-05-18 16:13:48 +0200

    rename file 2 functions added

---
+++ R/get_data.R
@@ -0,0 +1,25 @@
+#'
+#'
+#'
+#'
+#'
+#'
+
+getMSigDB <- function(){
+
+}
+
+#'
+#'
+#'
+#'
+#'
+#'
+
+getHCOP <- function(species){
+
+  path('ftp://ftp.ebi.ac.uk/pub/databases/genenames/hcop/')
+
+  paste(species, '')
+
+}
